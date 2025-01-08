831e1621d5827143cf74d5b1d0af40ce2aaca469
Risyandi <risyandi.94@gmail.com>
2025-01-08 22:40:01 +0700

    fix(error): updated text and solved error tag

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -15,21 +15,33 @@ export default function Home() {
 
     try {
       // Example API calls for fetching thumbnail and metadata
-      const thumbnailResponse = await axios.post("http://localhost:8000/api/v1/video/generator", {
-        videoUrl: videoUrl,
-        frameCount: 3
-      });
-      console.log("risyandi ~ handleFetchDetails ~ thumbnailResponse:", thumbnailResponse)
-      const metadataResponse = await axios.post("http://localhost:8000/api/v1/video/extractor", {
-        videoUrl: videoUrl,
-      });
-      console.log("risyandi ~ handleFetchDetails ~ metadataResponse:", metadataResponse)
+      const thumbnailResponse = await axios.post(
+        "http://localhost:8000/api/v1/video/generator",
+        {
+          videoUrl: videoUrl,
+          frameCount: 3,
+        }
+      );
+      console.log(
+        "risyandi ~ handleFetchDetails ~ thumbnailResponse:",
+        thumbnailResponse
+      );
+      const metadataResponse = await axios.post(
+        "http://localhost:8000/api/v1/video/extractor",
+        {
+          videoUrl: videoUrl,
+        }
+      );
+      console.log(
+        "risyandi ~ handleFetchDetails ~ metadataResponse:",
+        metadataResponse
+      );
 
       setThumbnailUrl(thumbnailResponse.data.thumbnailPath);
       setMetadata(metadataResponse.data.metadata);
     } catch (err) {
       setError(
-        "Failed to fetch video details. Please check the URL or try again."
+        "Failed to generate video. Please check the URL or try again."
       );
     }
   };
@@ -38,21 +50,21 @@ export default function Home() {
     <div>
       <div className="min-h-screen bg-gray-100 p-8">
         <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg p-6">
-          <h1 className="text-2xl font-bold mb-4">Vidgenex</h1>
+          <h1 className="text-2xl font-bold mb-4">Video Generator</h1>
           <input
             type="text"
             value={videoUrl}
             onChange={(e) => setVideoUrl(e.target.value)}
-            placeholder="Enter video URL"
+            placeholder="http://example.com/video.mp4"
             className="w-full p-2 border rounded-md mb-4 focus:outline-none focus:ring focus:ring-indigo-300"
           />
           <button
             onClick={handleFetchDetails}
             className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600"
           >
-            Fetch Details
+            Generate
           </button>
-          {error && <p className="mt-4 text-red-500">{error}</p>}
+          {error && <div className="mt-4 text-red-500">{error}</div>}
 
           {thumbnailUrl && (
             <div className="mt-6">
@@ -71,9 +83,11 @@ export default function Home() {
           {metadata && (
             <div className="mt-6">
               <h2 className="font-semibold">Metadata</h2>
-              <textarea className="bg-gray-100 p-4 mt-2 text-sm resize-none rounded-md w-full h-52" value={JSON.stringify(metadata, null, 2)}
-              onChange={(e) => setMetadata(e.target.value)}>
-              </textarea>
+              <textarea
+                className="bg-gray-100 p-4 mt-2 text-sm resize-none rounded-md w-full h-52"
+                value={JSON.stringify(metadata, null, 2)}
+                onChange={(e) => setMetadata(e.target.value)}
+              ></textarea>
             </div>
           )}
         </div>
